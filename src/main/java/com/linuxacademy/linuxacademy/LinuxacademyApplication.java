65e19f50eef5623d35c19b37ee493a13a446bde0
Vipul Patil <gvipulpatil231@gmail.com>
2020-05-07 15:40:43 +0530

    test1

---
+++ src/main/java/com/linuxacademy/linuxacademy/LinuxacademyApplication.java
@@ -0,0 +1,13 @@
+package com.linuxacademy.linuxacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxacademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinuxacademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linuxacademy/linuxacademy/LinuxacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linuxacademy.linuxacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxacademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
